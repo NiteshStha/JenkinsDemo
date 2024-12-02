f10891924387efa347ab992028092898dcacc7a3
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2024-12-02 17:47:48 +0545

    Unit tests

---
+++ JenkinsDemo.UnitTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using JenkinsDemo.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsDemo.UnitTests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests : IDisposable
+    {
+        private HomeController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new HomeController();
+        }
+
+        [Test]
+        public void Index()
+        {
+            var result = _controller.Index();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
